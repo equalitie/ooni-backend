7989945cba1c9132691a79cee2ab6b8bd2f6876d
Ivan Vilata-i-Balaguer <ivan@selidor.net>
2017-06-13 15:45:36 +0200

    get local address from peer (besides port), provide it back
    This change allows the helper to accept both the old ``PORT`` and the new
    ``ADDRESS:PORT``, where ``ADDRESS`` is expected to be the local outgoing
    interface address of the established connection as the peer sees it, i.e. not
    as the helper sees it, since it may have been changed by NAT.
    
    The new information is saved locally as ``PUB_ADDR:PORT/?l=LOC_ADDR`` if
    there's not an identical entry in the file.  A similarly formatted entry may
    be provided back to the peer.  When the peer builds the URL (even if it knows
    nothing about local addresses), the URL will reflect both the public and local
    addresses of the other peer, which should help telling apart connections not
    working because of NAT (a normal case) or because of other reasons.
    
    Old ``PUB_ADDR:PORT`` entries are kept and may also be returned to peers.

--- oonib/testhelpers/peer_locator_helpers.py
+++ oonib/testhelpers/peer_locator_helpers.py
@@ -4,6 +4,11 @@ from oonib.config import config
 from oonib import log
 
 import random
+import re
+
+_max_data_len = len('[0123:4567:89ab:cdef:0123:4567:89ab:cdef]:65535')
+# Accept ``PORT`` or ``ADDR:PORT`` with IPv4 ``ADDR`` or IPv6 ``[ADDR]``.
+_data_re = re.compile('^(?:([.0-9]+|\[[:0-9a-fA-F]+\]):)?([0-9]+)$')
 
 class PeerLocatorProtocol(Protocol):
     """
@@ -11,7 +16,16 @@ class PeerLocatorProtocol(Protocol):
     and send another pair in response
     """
     def dataReceived(self, data):
-        self_peer = random_peer = '%s:%s' % (self.transport.getPeer().host, data)
+        match = _data_re.match(data[:_max_data_len])  # protect against garbage
+        if not match:
+            return
+        (addr, port) = match.groups()
+
+        self_peer = self_peer_short = '%s:%s' % (self.transport.getPeer().host, port)
+        if addr:
+            self_peer += ('/?l=%s' % addr)  # add local address
+        random_peer = self_peer
+
         log.msg("registering: %s" % self_peer)
         try:
             with open(config.helpers['peer-locator'].peer_list, 'a+') as peer_list_file:
@@ -26,14 +40,20 @@ class PeerLocatorProtocol(Protocol):
 
                 log.msg(str(peer_list))
                 log.msg("choosing a random peer from pool of %d peers" % peer_pool_size)
-                while(peer_pool_size > 1 and random_peer == self_peer):
+                # The file may contain ``PUB_ADDR:PORT`` or ``PUB_ADDR:PORT/?l=LOC_ADDR`` entries,
+                # do not return any entry with the same ``PUB_ADDR:PORT``.
+                random_peer_short = self_peer_short
+                while(peer_pool_size > 1 and random_peer_short == self_peer_short):
+                    # If the list only contains the sort and a long version of the same peer,
+                    # this will loop forever.  However this should be a very rare case.
                     random_peer = random.choice(peer_list)
+                    random_peer_short = random_peer.split('/')[0]
 
         except IOError as e:
             log.msg("IOError %s" % e)
 
         log.msg("seeding peer %s to peer %s" % (random_peer, self_peer))
-        if (random_peer == self_peer):
+        if (random_peer_short == self_peer_short):
             random_peer = ''
 
         self.transport.write(random_peer)
